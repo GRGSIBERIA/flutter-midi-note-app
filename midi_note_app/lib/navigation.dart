f56b43942b729158771aa0c80aac6c8608845943
Eiichi Takebuchi <nanashi4129@gmail.com>
2019-12-22 22:54:16 +0900

    エラーで動かないことはよくわかった

--- midi_note_app/lib/navigation.dart
+++ midi_note_app/lib/navigation.dart
@@ -10,19 +10,87 @@ class Navigation extends StatefulWidget
 
 class _Navigation extends State
 {
+  int _selectedIndex = 0;
+  List<BottomNavigationBarItem> _bottomNavigationBarItems = [];
+
+  static const _navigationIcons = [
+    Icons.home,
+    Icons.home,
+    Icons.home,
+    Icons.info
+  ];
+
+  static const _navigationItemNames = [
+    "周波数",
+    "ノート番号",
+    "その他",
+    "情報"
+  ];
+
+  @override
+  void initState()
+  {
+    super.initState();
+    _bottomNavigationBarItems.add(_updateActiveState(0));
+    for (var i = 1; i < _navigationItemNames.length; i++)
+    {
+      _bottomNavigationBarItems.add(_updateDeactiveState(i));
+    }
+  }
+
+  BottomNavigationBarItem _updateActiveState(int index) 
+  {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        _navigationIcons[index],
+        color: Colors.black87
+      ),
+      title: Text(
+        _navigationItemNames[index],
+        style: TextStyle(
+          color: Colors.black87
+        )
+      )
+    );
+  }
+
+  BottomNavigationBarItem _updateDeactiveState(int index)
+  {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        _navigationIcons[index],
+        color: Colors.black26
+      ),
+      title: Text(
+        _navigationItemNames[index],
+        style: TextStyle(
+          color: Colors.black26
+        )
+      )
+    );
+  }
+
+  void _onItemTapped(int index)
+  {
+    setState(() {
+      _bottomNavigationBarItems[_selectedIndex] = _updateDeactiveState(_selectedIndex);
+      _bottomNavigationBarItems[index] = _updateActiveState(index);
+      _selectedIndex = index;
+    });
+  }
+
+  List<BottomNavigationBarItem> _itemsResult()
+  {
+    return _bottomNavigationBarItems;
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text("周波数")
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text("ノート番号")
-        )
-      ]
+      type: BottomNavigationBarType.fixed,
+      items: _itemsResult(),
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
     );
   }
 }
